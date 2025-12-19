59abaf8fade45d7e7570843b38912f3c77f5bf4f
Invigo-Info <john.mayer0206@gmail.com>
2025-12-19 16:53:02 +0530

    Refactor font imports and add font weights/styles

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,16 +1,12 @@
 // src/app/ui/fonts.ts
-import localFont from 'next/font/local';
+import localFont from 'next/font/local'
 
 export const satoshi = localFont({
   src: [
-    {
-      path: '../../public/fonts/Satoshi.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    
-    // Add other weights and styles as needed
+    { path: '../../public/fonts/Satoshi-Regular.otf', weight: '400', style: 'normal' },
+    { path: '../../public/fonts/Satoshi-Medium.otf', weight: '500', style: 'normal' },
+    { path: '../../public/fonts/Satoshi-Bold.otf', weight: '700', style: 'normal' }
   ],
-  display: 'swap', // Ensures text is visible while the font is loading
-  variable: '--font-satoshi', // Optional: for use with CSS variables (e.g., in Tailwind CSS)
-});
+  display: 'swap',
+  variable: '--font-satoshi'
+})
