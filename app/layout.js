5d89dddd66e66c0246ba68eae1ddbef6f1812f42
Invigo-Info <john.mayer0206@gmail.com>
2025-12-19 16:52:23 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 
 import './globals.css'
-import { satoshi } from './ui/fonts';
+import { satoshi } from './ui/fonts'
 
 export const metadata = {
   title: 'AI-Powered Palm Reader | Unlock Your Future',
@@ -10,13 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={satoshi.className}>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=sathosi+Display:wght@400;500;600&display=swap" rel="stylesheet" />
-      </head>
-      <body>{children}</body>
+    <html lang="en" >
+      <body className={satoshi.className}>{children}</body>
     </html>
   )
 }
